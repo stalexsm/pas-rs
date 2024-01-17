06320743c87fe16f4f43ad8b1059b3c6ec74fd43
stalexsm <stalex.info@yandex.ru>
2024-01-17 15:19:23 +0300

    feat: Добавил возможность просмотра пароля при изменении

--- frontend/src/components/header/modal.rs
+++ frontend/src/components/header/modal.rs
@@ -18,6 +18,25 @@ pub fn modal(props: &Props) -> Html {
     let passwd2 = use_state_eq(String::new);
     let matched_passwd = use_state_eq(|| false);
 
+    let show_passwd1 = use_state_eq(|| false);
+    let show_passwd2 = use_state_eq(|| false);
+
+    let onclick_passwd1 = {
+        let cloned_show_passwd1 = show_passwd1.clone();
+        Callback::from(move |e: MouseEvent| {
+            e.prevent_default();
+            cloned_show_passwd1.set(!*cloned_show_passwd1);
+        })
+    };
+
+    let onclick_passwd2 = {
+        let cloned_show_passwd2 = show_passwd2.clone();
+        Callback::from(move |e: MouseEvent| {
+            e.prevent_default();
+            cloned_show_passwd2.set(!*cloned_show_passwd2);
+        })
+    };
+
     let onchange_passwd1 = {
         let cloned_passwd1 = passwd1.clone();
         let cloned_passwd2 = passwd2.clone();
@@ -88,38 +107,53 @@ pub fn modal(props: &Props) -> Html {
                                 <label for="passwd1" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">
                                     {"Пароль"}
                                 </label>
-                                <input
-                                    onchange={onchange_passwd1}
-                                    required={true}
-                                    type="password"
-                                    id="passwd1"
-                                    pattern={"[0-9a-zA-Z@!&^_.]{8,}"}
-                                    class={classes!(
-                                        "mb-5",
-                                        "mt-2",
-                                        "text-gray-600",
-                                        "focus:outline-none",
-                                        "focus:border",
-                                        "focus:border-indigo-700",
-                                        "font-normal",
-                                        "w-full",
-                                        "h-10",
-                                        "flex",
-                                        "items-center",
-                                        "pl-3",
-                                        "text-sm",
-                                        "border-gray-300",
-                                        "rounded",
-                                        "border",
-                                        if !(*passwd1).is_empty() && !*matched_passwd {"border-red-500"} else {""}
-                                    )}
-                                    placeholder="Введите пароль"
-                                />
+                                <div class="relative">
+                                    <input
+                                        onchange={onchange_passwd1}
+                                        required={true}
+                                        type={if *show_passwd1 {"text"} else {"password"}}
+                                        id="passwd1"
+                                        pattern={"[0-9a-zA-Z@!&^_.]{8,}"}
+                                        class={classes!(
+                                            "mb-5",
+                                            "mt-2",
+                                            "text-gray-600",
+                                            "focus:outline-none",
+                                            "focus:border",
+                                            "focus:border-indigo-700",
+                                            "font-normal",
+                                            "w-full",
+                                            "h-10",
+                                            "flex",
+                                            "items-center",
+                                            "pl-3",
+                                            "text-sm",
+                                            "border-gray-300",
+                                            "rounded",
+                                            "border",
+                                            if !(*passwd1).is_empty() && !*matched_passwd {"border-red-500"} else {""}
+                                        )}
+                                        placeholder="Введите пароль"
+                                    />
+                                    <span onclick={onclick_passwd1} class="absolute inset-y-0 right-3.5 pl-3 flex items-center mt-auto text-gray-500 cursor-pointer">
+                                        if *show_passwd1 {
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                            </svg>
+                                        } else {
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                            </svg>
+                                        }
+                                    </span>
+                                </div>
                                 <label for="passwd2" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">{"Повторите пароль"}</label>
+                                <div class="relative">
                                 <input
                                     onchange={onchange_passwd2}
                                     required={true}
-                                    type="password"
+                                    type={if *show_passwd2 {"text"} else {"password"}}
                                     id="passwd2"
                                     pattern={"[0-9a-zA-Z@!&^_.]{8,}"}
                                     class={classes!(
@@ -143,6 +177,19 @@ pub fn modal(props: &Props) -> Html {
                                     )}
                                     placeholder="Введите пароль еще раз"
                                 />
+                                <span onclick={onclick_passwd2} class="absolute inset-y-0 right-3.5 pl-3 flex items-center mt-auto text-gray-500 cursor-pointer">
+                                    if *show_passwd2 {
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg>
+                                    } else {
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                        </svg>
+                                    }
+                                </span>
+                                </div>
                                 <div class="flex items-center justify-center w-full">
                                     <button
                                         onclick={props.toggle.clone()}
