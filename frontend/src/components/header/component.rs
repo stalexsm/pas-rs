235ec0f14da59bee9f730e82d56ac479a3060077
stalexsm <stalex.info@yandex.ru>
2024-01-31 10:52:45 +0300

    feat: Ссылка на главную по логотипу

--- frontend/src/components/header/component.rs
+++ frontend/src/components/header/component.rs
@@ -187,7 +187,9 @@ pub fn header() -> Html {
                     </div>
                     <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div style="min-width: 40px;" class="flex flex-shrink-0 items-center">
-                            <img class="h-8 w-auto" src="./assets/img/logo.png" alt="PAS"/>
+                            <Link<Route> to={Route::Home}>
+                                <img class="h-8 w-auto" src="./assets/img/logo.png" alt="PAS"/>
+                            </Link<Route>>
                         </div>
                         <div class="hidden sm:ml-6 sm:block">
                             <div class="flex space-x-4">
