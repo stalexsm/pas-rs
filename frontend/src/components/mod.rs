8d674bf5adc4a6251b867487fe74eabe89640c94
stalexsm <stalex.info@yandex.ru>
2024-01-31 13:25:07 +0300

    feat: добавил хук по отслеживанию клика по элементу

--- frontend/src/components/mod.rs
+++ frontend/src/components/mod.rs
@@ -1,4 +1,7 @@
 use serde::{Deserialize, Serialize};
+use wasm_bindgen::{closure::Closure, JsCast};
+use web_sys::Node;
+use yew::prelude::*;
 
 pub mod analitic;
 pub mod auth;
@@ -24,4 +27,36 @@ pub trait SelectableItem {
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 pub struct ResponseError {
     pub detail: String,
+}
+
+#[hook]
+fn use_outside_click(node_ref: NodeRef, callback: Callback<MouseEvent>) {
+    // Функция для отслеживания клика вне элемента
+
+    use_effect_with(node_ref, move |node_ref| {
+        let document = web_sys::window().unwrap().document().unwrap();
+
+        let element = node_ref.get().unwrap();
+        let listener = {
+            let cloned_element = element.clone();
+            Closure::new(Box::new(move |event: web_sys::MouseEvent| {
+                let e = event.target().unwrap().dyn_into::<Node>().ok();
+                if !cloned_element.contains(e.as_ref()) {
+                    log::debug!("Run!!! use_outside_click");
+                    callback.emit(event);
+                }
+            }) as Box<dyn FnMut(_)>)
+        };
+
+        let closure = listener.as_ref().clone();
+        document
+            .add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())
+            .unwrap();
+
+        move || {
+            document
+                .remove_event_listener_with_callback("mousedown", listener.as_ref().unchecked_ref())
+                .unwrap();
+        }
+    });
 }
